577336c1d8b43f4e53ef0fde231ad786ad565f54
5gfhd2dd5m-ai <5gfhd2dd5m@privaterelay.appleid.com>
2026-09-04 17:43:38 +0200

    Create abi.js

---
+++ abi.js
@@ -0,0 +1,9 @@
+const erc20_abi = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function decimals() view returns (uint8)",
+    "function balanceOf(address) view returns (uint)",
+    "function approve(address spender, uint256 amount) returns (bool)",
+    "function transferFrom(address from, address to, uint256 value) returns (bool)"
+];
+
